e098934b0c3d32471fdc14840628c493489bb15f
CARDON <cardonkevin02@gmail.com>
2019-04-05 21:01:12 +0200

    Social login Facebook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,27 @@ import {HttpClientModule} from '@angular/common/http';
 import {MatPaginatorModule, MatRadioModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NgxPaginationModule} from 'ngx-pagination';
+import { SocialLoginModule, AuthServiceConfig } from 'angularx-social-login';
+import {FacebookLoginProvider} from 'angularx-social-login';
+import { AuthentificationComponent } from './control/authentification/authentification.component';
+
+
+const config = new AuthServiceConfig([
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider('370545117128079')
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllListingComponent
+    AllListingComponent,
+    AuthentificationComponent
   ],
   imports: [
     BrowserModule,
@@ -22,9 +38,15 @@ import {NgxPaginationModule} from 'ngx-pagination';
     MatPaginatorModule,
     BrowserAnimationsModule,
     NgxPaginationModule,
-    MatRadioModule
+    MatRadioModule,
+    SocialLoginModule
   ],
-  providers: [MangaService],
+  providers: [MangaService,
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/control/authentification/authentification.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService, FacebookLoginProvider, SocialUser} from 'angularx-social-login';
+
+@Component({
+  selector: 'app-authentification',
+  templateUrl: './authentification.component.html',
+  styleUrls: ['./authentification.component.scss']
+})
+export class AuthentificationComponent implements OnInit {
+
+  user: SocialUser;
+  loggedIn: boolean;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+}
